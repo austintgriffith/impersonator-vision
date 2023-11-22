724690df494951fed3693ca4bbf1ea08677eca5b
Austin Griffith <austin@ethereum.org>
2023-11-22 12:37:40 -0700

    sort of working but lots more changes to make

--- packages/nextjs/pages/_app.tsx
+++ packages/nextjs/pages/_app.tsx
@@ -43,7 +43,7 @@ const ScaffoldEthApp = ({ Component, pageProps }: AppProps) => {
           theme={isDarkTheme ? darkTheme() : lightTheme()}
         >
           <div className="flex flex-col min-h-screen">
-            <Header />
+            {/*<Header />*/}
             <main className="relative flex flex-col flex-1">
               <Component {...pageProps} />
             </main>
--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -1,24 +1,77 @@
+import React from "react";
 import { ImpersonatorIframe, useImpersonatorIframe } from "@impersonator/iframe";
 import type { NextPage } from "next";
+import { usePublicClient } from "wagmi";
 import { MetaHeader } from "~~/components/MetaHeader";
-
-//import { getTargetNetwork } from "~~/utils/scaffold-eth";
+import { AddressInput, InputBase } from "~~/components/scaffold-eth";
+import { getTargetNetwork } from "~~/utils/scaffold-eth";
 
 const Home: NextPage = () => {
   const { latestTransaction } = useImpersonatorIframe();
 
+  const [impersonateAddress, setImpersonateAddress] = React.useState<string>("");
+
+  const possibleNetworks = [
+    { name: "mainnet", rpcUrl: "https://cloudflare-eth.com" },
+    { name: "optimism", rpcUrl: " https://mainnet.optimism.io" },
+  ];
+
+  const [appUrl, setAppUrl] = React.useState<string>("https://app.uniswap.org/pools?chain=mainnet");
+
+  const handleAppUrlChange = (newValue: string) => {
+    setAppUrl(newValue);
+  };
+
+  const handleAddressChange = (newValue: string) => {
+    setImpersonateAddress(newValue);
+  };
+
+  const [selectedNetwork, setSelectedNetwork] = React.useState(possibleNetworks[0]);
+
+  const targetNetwork = getTargetNetwork();
+  console.log("targetNetwork", targetNetwork);
   return (
     <>
       <MetaHeader />
+      <div className="flex flex-col items-center justify-center p-8">
+        <h1 className="text-3xl font-bold">Impersonate</h1>
+        <AddressInput value={impersonateAddress} placeholder="vitalik.eth" onChange={handleAddressChange} />
+        <h1 className="text-3xl font-bold">at</h1>
+        <div className="w-[400px]">
+          <InputBase value={appUrl} onChange={handleAppUrlChange} />
+        </div>
+        <h1 className="text-3xl font-bold">on</h1>
+        <div className="w-[200px]">
+          <select
+            className="select select-bordered w-full max-w-xs "
+            onChange={e => {
+              setSelectedNetwork(possibleNetworks[e.target.selectedIndex]);
+            }}
+          >
+            {possibleNetworks.map(network => {
+              return (
+                <option key={network.name} value={network.rpcUrl}>
+                  {network.name}
+                </option>
+              );
+            })}
+          </select>{" "}
+        </div>
+      </div>
       <div className="flex items-center flex-col flex-grow pt-10">
         <div className={"border-2 border-gray-200 rounded-md p-4"}>
-          <ImpersonatorIframe
-            width={"800px"}
-            height={"500px"}
-            src="https://app.uniswap.org/pools?chain=mainnet"
-            address="0x1246956C9984EdA22A6C7C4Ea0A8c4499De016f4"
-            rpcUrl={"https://rpc.scaffoldeth.io:48544/"}
-          />
+          {selectedNetwork.rpcUrl && impersonateAddress && appUrl ? (
+            <ImpersonatorIframe
+              key={selectedNetwork.rpcUrl + impersonateAddress + appUrl}
+              width={"1400px"} //set it to the browser width
+              height={"1200px"}
+              src={appUrl}
+              address={impersonateAddress}
+              rpcUrl={selectedNetwork.rpcUrl}
+            />
+          ) : (
+            ""
+          )}
         </div>
         <div className="p-4">
           {latestTransaction ? (
